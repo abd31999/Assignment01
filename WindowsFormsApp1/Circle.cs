b2decce64ff85c55f3c378d1f2a8f84fbd789c16
abd31999 <abd31999@users.noreply.github.com>
2022-04-27 21:26:45 +0300

    0

---
+++ WindowsFormsApp1/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+   public class Circle
+    {
+        private int radius;
+        private string color;
+        public int Radius
+        {
+            get { return radius; }
+
+          private set { radius = value; }
+        }
+        public Circle(int r, string c)
+        {
+            radius = r;
+            color = c;
+        }
+        public double Calculate_Area()
+        {
+            return radius * radius * 3.14;
+        }
+        public double Calculate_circumference()
+        {
+            return radius * 3.14;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Circle C1 = new Circle(int.Parse(textBox1.Text), textBox2.Text);
+            MessageBox.Show("Your Circle Has Been Created");
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Circle C2 = new Circle(int.Parse(textBox1.Text), textBox2.Text);
+            textBox3.Text = C2.Calculate_Area().ToString();
+            textBox4.Text = C2.Calculate_circumference().ToString(); 
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
